78fe49ff524e5d068424e1059e4c2e9c67edb8e3
shitalajagekar <48644471+shitalajagekar@users.noreply.github.com>
2021-04-17 17:24:53 +0530

    Add files via upload
    to convert function into attribute with the help of of @property decorator and change the value of it's parameter using @(name).setter and delete it's value using deleter

---
+++ OOPSconcept5.py
@@ -0,0 +1,32 @@
+# use of decorator,setter
+
+class Employee:
+    def __init__(self,fname,lname):
+        self.fname= fname
+        self.lname= lname
+
+    @property
+    def email(self):
+        if self.fname==None and self.lname==None:
+            return "Email do not set"
+        else:
+            return self.fname + "."+ self.lname + "@gmail.com"
+
+    @email.setter
+    def email(self,givenmail):
+        split_id=givenmail.split('@')[0].split('.')
+        self.fname= split_id[0]
+        self.lname=split_id[1]
+
+    @email.deleter
+    def email(self):
+        self.fname=None
+        self.lname=None
+
+
+Neha = Employee("Neha", " Chavan")
+Neha.email="Chavan.Neha@gmail.com"
+print(Neha.email)
+
+del Neha.email
+print(Neha.email)
