4fead5f8246fcbba76ae760218d401c4a5d2b07e
shitalajagekar <48644471+shitalajagekar@users.noreply.github.com>
2021-04-29 23:42:08 +0530

    Add files via upload
    print line number of string present in log file

--- Read_logfile.py
+++ Read_logfile.py
@@ -1,6 +1,6 @@
 #   read log file in python and check  " python" word is present or not and display the message.
 
-with open("log_file.txt") as f:
+"""with open("log_file.txt") as f:
     content= f.read()
 
 if "python" in content.lower():
@@ -14,3 +14,17 @@ if "EDC8112I" in content:
     print("EDC8112I is present")
 else:
     print("EDC8112I is not present")
+    
+"""
+
+content=" "
+i=1
+with open("log_file.txt") as f:
+    while content:
+       
+        content= f.readline()
+        if "EDC8112I" in content:
+            print("EDC8112I is present")
+            print(i)
+        i+=1
+        
